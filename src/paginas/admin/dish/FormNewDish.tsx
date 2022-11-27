200faa70fce52d268d95e8a992f7311e30204398
Gabriel Martins <87931081+gdlmartins@users.noreply.github.com>
2022-11-27 18:44:30 +0800

    new dishes with pic

--- src/paginas/admin/dish/FormNewDish.tsx
+++ src/paginas/admin/dish/FormNewDish.tsx
@@ -14,12 +14,40 @@ interface TAGS {
 }
 
 const FormNewDish = () => {
-    const [dish, setDish] = useState<string>("");
-    const [restaurant, setRestaurant] = useState<IRestaurante[]>([]);
+    const [restaurants, setRestaurants] = useState<IRestaurante[]>([]);
     const [tags, setTags] = useState<TAG[]>();
+    
+    const [dish, setDish] = useState<string>("");
+    const [tag , setTag] = useState<string>("");
+    const [description , setDescription] = useState<string>("");
+    const [restaraunt , setRestaraunt] = useState<string>("");
 
+    const [imagem , setImagem] = useState<File | null >(null)
+
+    const addDish = (data: FormData)=> {
+        httpV2.request({
+            url: "pratos/",
+            method: "POST",
+            headers: {
+                "Content-Type" : "multipart/form-data"
+            },
+            data: data 
+        }).then(()=> alert("Adicionado"))
+        .catch((e)=> console.log(e))
+    }
+    
     const submitHanler = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        const formData = new FormData();
+        formData.append("nome", dish)
+        formData.append("tag", tag)
+        formData.append("descricao",description )
+        formData.append("restaurante",restaraunt )
+   
+        if(imagem) formData.append("imagem", imagem)
+
+        addDish(formData);
+   
     }
 
     const getCategories = async () => {
@@ -28,8 +56,16 @@ const FormNewDish = () => {
     }
     const getRestaurantes = async () => {
         const resp = await httpV2.get<IRestaurante[]>("restaurantes/")
-        setRestaurant(resp.data)
+        setRestaurants(resp.data)
     }
+
+    const selectImage = (e: React.ChangeEvent<HTMLInputElement> )=>{
+        if(e.target.files?.length){
+            setImagem(e.target.files[0])
+        }else setImagem(null)
+
+    }
+
 
     useEffect(() => {
         getCategories()
@@ -58,9 +94,7 @@ const FormNewDish = () => {
                         onChange={(e) => setDish(e.target.value)}
                         margin="dense"
                     />
-                    <FormControl
-                        margin="dense"
-                        fullWidth>
+                    <FormControl fullWidth>
                         <InputLabel id="Selection">Category</InputLabel>
                         <Select
                             label="Selection"
@@ -70,8 +104,8 @@ const FormNewDish = () => {
                             fullWidth
                             required
                             variant='standard'
-                        // value={dish}
-                        // onChange={(e) => setDish(e.target.value)}
+                            value={tag}
+                           onChange={(e) => setTag(e.target.value)}
                         >{tags?.map(tag =>
                             <MenuItem key={tag.id} value={tag.value}>{tag.value}</MenuItem>
                         )}
@@ -82,12 +116,10 @@ const FormNewDish = () => {
                         fullWidth
                         required
                         variant='standard'
-                        value={dish}
-                        onChange={(e) => setDish(e.target.value)}
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
                     />
-                    <FormControl
-                        margin="dense"
-                        fullWidth>
+                    <FormControl fullWidth>
                         <InputLabel id="Restaurante">Restaurante</InputLabel>
                         <Select
                             label="Restaurante"
@@ -97,22 +129,22 @@ const FormNewDish = () => {
                             margin="dense"
                             fullWidth
                             required
+                            value={restaraunt}
                             variant='standard'
+                            onChange={(e) => setRestaraunt(e.target.name)}
                         >
-                            {restaurant?.map(tag => <MenuItem
-                                key={tag.id}
-                                value={tag.id}>
-                                {tag.nome}
+                            {restaurants?.map(restaraunt => <MenuItem
+                                key={restaraunt.id}
+                                value={restaraunt.id}>
+                                {restaraunt.nome}
                             </MenuItem>
                             )}
-                            {/* // onChange={(e) => setDish(e.target.value */}
                         </Select>
                     </FormControl>
-                    <TextField
-                        margin="dense"
-                        fullWidth
+                    <input
+                        onChange={ e => selectImage(e)}
                         type="file"
-                    ></TextField>
+                    ></input>
                     <Button variant="outlined"
                         fullWidth
                         type="submit"
